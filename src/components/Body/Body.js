e134334ae0d8d387899fc43d1ea46f2e15e28928
Polina Kulik <pollyandrrrr@yandex.ru>
2023-10-26 00:48:26 +0300

    add regexp '-', '0', '123'

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,16 +3,26 @@ import './Body.css';
 import React, { useState } from 'react';
 
 function Body() {
-    const [min, setMin] = useState();
-    const [max, setMax] = useState();
-    const [num, setNum] = useState();
+    const [min, setMin] = useState('');
+    const [max, setMax] = useState('');
+    const [num, setNum] = useState('');
 
-    function onMin(event) {
-        setMin(Number.parseInt(event.target.value));
+    function onMinChange(event) {
+        const inputValue = event.target.value;
+        if(!inputValue || inputValue.match(/^((-?[1-9][0-9]*)|0|-)$/g)) {
+            setMin(inputValue);
+        } else {
+            setMin(min);
+        }
     }
 
-    function onMax(event) {
-        setMax(Number.parseInt(event.target.value));
+    function onMaxChange(event) {
+        const inputValue = event.target.value;
+        if(!inputValue || inputValue.match(/^((-?[1-9][0-9]*)|0|-)$/g)) {
+            setMax(inputValue);
+        } else {
+            setMax(max);
+        }
     }
 
     function getRandomNumber(min, max) {
@@ -20,8 +30,10 @@ function Body() {
     };
 
     function handleClick() {
-        if (min !== undefined && max !== undefined && min < max) {
-            setNum(getRandomNumber(min, max));
+        const minNum = Number.parseInt(min)
+        const maxNum = Number.parseInt(max)
+        if (min !== '' && max !== '' && minNum < maxNum) {
+            setNum(getRandomNumber(minNum, maxNum));
         } else {
             setNum('error');
         }
@@ -32,11 +44,11 @@ function Body() {
             <section className='body'>
                 <div className='body__group'>
                     <p className='body__text'>min</p>
-                    <input className='body__input' type="number" value={min} onInput={onMin}/>
+                    <input className='body__input' type="text" min='-10000000' value={min} onChange={onMinChange} />
                 </div>
                 <div className='body__group'>
                     <p className='body__text'>max</p>
-                    <input className='body__input' type="number" value={max} onInput={onMax}/>
+                    <input className='body__input' type="number" min='-10000000' value={max} onChange={onMaxChange}/>
                 </div>
                 <button className='body__btn' onClick={handleClick}>press</button>
                 <div className='body__group'>
